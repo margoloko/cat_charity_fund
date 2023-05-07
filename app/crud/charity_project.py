26f1bc107a82030e8ba75b837e91912aa65b4856
1 <margoloko@mail.ru>
2023-05-07 16:03:32 +0700

    add get_projects_by_completion_rate

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -15,9 +15,19 @@ class CRUDCharityProject(CRUDBase):
         db_project_id = await session.execute(select(CharityProject.id).where(CharityProject.name == name))
         return db_project_id.scalars().first()
 
-    async def get_projects_by_completion_rate():
-        #projects = await
-        pass
+    async def get_projects_by_completion_rate(self,
+                                              session: AsyncSession):
+        """Получает отсортированый список закрытых проектов."""
+        projects = await session.execute(select(CharityProject).where(CharityProject.fully_invested))
+
+        projects = projects.scalars().all()
+        closed_projects = []
+        for project in projects:
+            closed_projects.append({'name': project.name,
+                                    'description': project.description,
+                                    'close_time': project.close_date - project.create_date,})
+        return sorted(closed_projects, key=lambda time: time['close_time'])
+
 
 
 project_crud = CRUDCharityProject(CharityProject)
